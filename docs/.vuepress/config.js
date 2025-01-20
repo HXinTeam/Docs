96ef10d6fd8080518483962804d228482bc99af3
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2025-01-20 16:27:07 +0800

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+// import { webpackBundler } from '@vuepress/bundler-webpack'
+import { recoTheme } from 'vuepress-theme-reco'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  base: "/",
+  lang: "zh-cn",
+  title: "幻心互联V3帮助文档",
+  description: "适用于 幻心互联V3 的帮助文档",
+  //head: [["link", { rel: "icon", href: "https://cdn.qwqo.cn" }]],
+  head: [],
+  bundler: viteBundler(),
+  theme: recoTheme({
+    navbar: [
+      {
+        text: "首页",
+        link: "/main",
+      },
+      {
+        text: "控制台",
+        link: "https://app.2mc.shop",
+      },
+      "/contact",
+    ],
+    series: {
+      "/": [
+        "/main",
+        "/contact",
+        "/start",
+        "/solution",
+        "/definition",
+        {
+          text: "账户",
+          children: ["/account/main"],
+        },
+      ],
+    },
+  }),
+});
